e072248b38e362c10c883bcc898dedbfd7241288
Rishabh <rishabh09p@gmail.com>
2024-08-01 01:55:41 +0530

    Fixes

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function toggleMenu() {
   hamburger.classList.toggle('active');
   menu.classList.toggle('activeMenu');
 }
+function loadComponent(id, url) {
+  fetch(url)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(id).innerHTML = data;
+      })
+      .catch(error => console.error('Error loading component:', error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('navbar-container', '/src/components/Navbar/navbar.html');
+  loadComponent('main-container', '/src/components/Hero Section/main.html');
+  loadComponent('footer-container', '/src/components/Footer/footer.html');
+});
--- src/loadScript.js
+++ src/loadScript.js
@@ -1,14 +1,14 @@
-function loadComponent(id, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        })
-        .catch(error => console.error('Error loading component:', error));
-}
+// function loadComponent(id, url) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById(id).innerHTML = data;
+//         })
+//         .catch(error => console.error('Error loading component:', error));
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadComponent('navbar-container', '/src/components/Navbar/navbar.html');
-    loadComponent('main-container', '/src/components/Hero Section/main.html');
-    loadComponent('footer-container', '/src/components/Footer/footer.html');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     loadComponent('navbar-container', '/src/components/Navbar/navbar.html');
+//     loadComponent('main-container', '/src/components/Hero Section/main.html');
+//     loadComponent('footer-container', '/src/components/Footer/footer.html');
+// });
