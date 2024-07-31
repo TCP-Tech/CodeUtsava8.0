e4c67cb71a63f985062749b990c56f68290a8d8c
Rishabh <rishabh09p@gmail.com>
2024-08-01 02:21:16 +0530

    Fixes

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ function toggleMenu() {
   hamburger.classList.toggle('active');
   menu.classList.toggle('activeMenu');
 }
-function loadComponent(id, url) {
-  fetch(url)
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById(id).innerHTML = data;
-      })
-      .catch(error => console.error('Error loading component:', error));
-}
-
+// function loadComponent(id, url) {
+//   fetch(url)
+//       .then(response => response.text())
+//       .then(data => {
+//           document.getElementById(id).innerHTML = data;
+//       })
+//       .catch(error => console.error('Error loading component:', error));
+// }
 document.addEventListener('DOMContentLoaded', () => {
+  const loadComponent = async (targetId, url) => {
+    try {
+      const response = await fetch(url);
+      const text = await response.text();
+      const target = document.getElementById(targetId);
+      if (target) {
+        target.innerHTML = text;
+      }
+    } catch (error) {
+      console.error(`Error loading component from ${url}:`, error);
+    }
+  };
+
   loadComponent('navbar-container', '/src/components/Navbar/navbar.html');
   loadComponent('main-container', '/src/components/Hero Section/main.html');
   loadComponent('footer-container', '/src/components/Footer/footer.html');
