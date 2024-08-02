0b352d4475047eacfbebd55e82ad34e3b7d06c93
ArunRathaur1 <arunrathaur92.6@gmail.com>
2024-08-03 03:04:18 +0530

    updated the changes

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-
+  
   const hamburger = document.querySelector('.ham');
   const menu = document.getElementById('hamburgerMenu');
   hamburger.classList.toggle('active');
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error(`Error loading component from ${url}:`, error);
     }
   };
+
   loadComponent('navbar-container', '/components/Navbar/navbar.html');
   loadComponent('main-container', '/components/Hero Section/main.html');
   loadComponent('footer-container', '/components/Footer/footer.html');
