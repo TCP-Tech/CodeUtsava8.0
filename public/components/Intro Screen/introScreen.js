8a77e4f88d71834253d08a909eb60ef2ca0e442f
Rishabh <rishabh09p@gmail.com>
2024-08-03 01:54:20 +0530

    Fixes in intro

--- public/components/Intro Screen/introScreen.js
+++
@@ -1,8 +0,0 @@
-const introButton = document.getElementById('intro-button');
-const leftCloud = document.getElementById('leftCloud');
-const rightCloud = document.getElementById('rightCloud');
-
-introButton.addEventListener('click', function() {
-    leftCloud.classList.add('animation-left-cloud');
-    rightCloud.classList.add('animation-right-cloud');
-});
