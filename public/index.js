7ee1140894859eadfd25056ff586ce88b3dcc112
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-08-03 02:49:37 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+
+  const hamburger = document.querySelector('.ham');
+  const menu = document.getElementById('hamburgerMenu');
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('activeMenu');
+}
 document.addEventListener('DOMContentLoaded', () => {
   const loadComponent = async (targetId, url) => {
     try {
