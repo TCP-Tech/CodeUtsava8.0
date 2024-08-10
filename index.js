a91e63d2b2db6509a15505a40507740d4835d262
karanmanglani <bkn45for@gmail.com>
2024-08-10 11:54:10 +0530

    Fixed Navbar

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   function toggleMenu() {
-    const hamburger = document.querySelector('.ham');
-    const menu = document.getElementById('hamburgerMenu');
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('activeMenu');
+    const hamburger = document.querySelector(".ham");
+    const menu = document.getElementById("hamburgerMenu");
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("activeMenu");
   }
 
   const loadComponent = (url, targetId) => {
     fetch(url)
-      .then(response => response.text())
-      .then(data => {
+      .then((response) => response.text())
+      .then((data) => {
         const target = document.getElementById(targetId);
         if (target) {
           target.innerHTML = data;
         }
       })
-      .catch(error => console.error('Error loading component:', error));
+      .catch((error) => console.error("Error loading component:", error));
   };
 
-  const introButton = document.querySelector('.intro-button');
-  const introBG = document.querySelector('.image-cont');
-  const introScreen = document.querySelector('.intro-screen');
-  const cloudsContainer = document.querySelector('.clouds-container');
-  const mainContent = document.getElementById('main-content');
-  
+  const introButton = document.querySelector(".intro-button");
+  const introBG = document.querySelector(".image-cont");
+  const introScreen = document.querySelector(".intro-screen");
+  const cloudsContainer = document.querySelector(".clouds-container");
+  const mainContent = document.getElementById("main-content");
 
-  const cloudSound = document.getElementById('cloudSound');
-  const backgroundMusic = document.getElementById('backgroundMusic');
-  const buttonHoverSound = document.getElementById('buttonHoverSound');
-  const playPauseButton = document.getElementById('playPauseButton');
-  const playButtonImage = document.getElementById('play-button-image');
-  const enterButton = document.getElementById('intro-btn');
+  const cloudSound = document.getElementById("cloudSound");
+  const backgroundMusic = document.getElementById("backgroundMusic");
+  const buttonHoverSound = document.getElementById("buttonHoverSound");
+  const playPauseButton = document.getElementById("playPauseButton");
+  const playButtonImage = document.getElementById("play-button-image");
+  const enterButton = document.getElementById("intro-btn");
 
   function loadContents() {
-    cloudsContainer.classList.add('show');
+    cloudsContainer.classList.add("show");
     setTimeout(() => {
-      mainContent.style.display = 'block';
-      loadComponent('/components/Navbar/navbar.html', 'navbar-container');
-      loadComponent('/components/Footer/footer.html', 'footer-container');
-      loadComponent('/components/Hero Section/main.html', 'main-container');
-    }, 0); 
+      mainContent.style.display = "block";
+      loadComponent("/components/Navbar/navbar.html", "navbar-container");
+      loadComponent("/components/Footer/footer.html", "footer-container");
+      loadComponent("/components/Hero Section/main.html", "main-container");
+    }, 0);
     setTimeout(() => {
-      cloudsContainer.classList.remove('show');
-      cloudsContainer.classList.add('hide');
+      cloudsContainer.classList.remove("show");
+      cloudsContainer.classList.add("hide");
       setTimeout(() => {
-        introButton.style.display = 'none'; 
-        introBG.style.display = 'none'; 
-      }, 0); 
+        introButton.style.display = "none";
+        introBG.style.display = "none";
+      }, 0);
       setTimeout(() => {
-        introScreen.style.display = 'none';
+        introScreen.style.display = "none";
       }, 3000);
-    }, 3000);  
+    }, 3000);
+
+    setTimeout(() => {
+      const hamburg = document.querySelector(".hamburger");
+      hamburg.addEventListener("click", toggleMenu);
+    }, 3500);
   }
 
-  introButton.addEventListener('click', loadContents);
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
+  introButton.addEventListener("click", loadContents);
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
       loadContents();
     }
-    if (event.key === 'M' || event.key == 'm') {
+    if (event.key === "M" || event.key == "m") {
       handlePlayPause();
     }
   });
 
   function playCloudSound() {
-    cloudSound.play().catch(error => console.error('Error playing cloud sound:', error));
+    cloudSound
+      .play()
+      .catch((error) => console.error("Error playing cloud sound:", error));
   }
 
   function handleEnterButtonClick() {
@@ -73,24 +79,34 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(backgroundMusic);
     if (buttonHoverSound.paused) {
       buttonHoverSound.currentTime = 0;
-      buttonHoverSound.play().catch(error => console.error('Error playing button sound:', error));
+      buttonHoverSound
+        .play()
+        .catch((error) => console.error("Error playing button sound:", error));
       setTimeout(() => {
-        backgroundMusic.play().catch(error => console.error('Error playing background music:', error));
+        backgroundMusic
+          .play()
+          .catch((error) =>
+            console.error("Error playing background music:", error)
+          );
       }, 3000);
     }
     setTimeout(playCloudSound, 1500);
   }
 
-  enterButton.addEventListener('click', handleEnterButtonClick);
+  enterButton.addEventListener("click", handleEnterButtonClick);
 
   function handlePlayPause() {
     if (backgroundMusic.paused) {
-      backgroundMusic.play().catch(error => console.error('Error starting background music:', error));
-      playButtonImage.setAttribute('src', '/assets/images/playing.svg');
+      backgroundMusic
+        .play()
+        .catch((error) =>
+          console.error("Error starting background music:", error)
+        );
+      playButtonImage.setAttribute("src", "/assets/images/playing.svg");
     } else {
       backgroundMusic.pause();
-      playButtonImage.setAttribute('src', '/assets/images/mute.svg');
+      playButtonImage.setAttribute("src", "/assets/images/mute.svg");
     }
   }
-  playPauseButton.addEventListener('click', handlePlayPause);
+  playPauseButton.addEventListener("click", handlePlayPause);
 });
--- public/components/Navbar/navbar.js
+++ public/components/Navbar/navbar.js
@@ -1,9 +1,9 @@
-const hamburg = document.querySelector('.hamburger')
+// const hamburg = document.querySelector(".hamburger");
 function toggleMenu() {
-    const hamburger = document.querySelector('.ham');
-    const menu = document.getElementById('hamburgerMenu');
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('activeMenu');
-  }
+  const hamburger = document.querySelector(".ham");
+  const menu = document.getElementById("hamburgerMenu");
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("activeMenu");
+}
 
-  hamburg.addEventListener('click' , toggleMenu)
+// hamburg.addEventListener("click", toggleMenu);
