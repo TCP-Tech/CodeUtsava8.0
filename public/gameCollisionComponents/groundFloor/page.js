ae6dbb5fdb7c481b3e51c4a55cba4d3b64f7ad5b
Rishabh <rishabh09p@gmail.com>
2024-09-24 03:47:55 +0530

    fix

--- public/gameCollisionComponents/groundFloor/page.js
+++ public/gameCollisionComponents/groundFloor/page.js
@@ -1,4 +1,4 @@
-import { handleFaqButtonClick } from "../../router";
+import { handleFaqButtonClick } from "../../router.js";
 document.addEventListener("modalDisplayed",()=>{
     console.log("Faq" , document.getElementById('navigateToFaq'));
     document.getElementById('navigateToFaq').addEventListener('click', handleFaqButtonClick);
